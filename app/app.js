8c646f23c1e0b7bac3646330ff81d1c850970d45
Volodymyr Pavlenko <vpavlenko@cogniance.com>
2016-08-10 00:14:05 +0300

    Integrate angular google gapi

--- app/app.js
+++ app/app.js
@@ -2,11 +2,42 @@
 
 // Declare app level module which depends on views, and components
 angular.module('gmailChecker', [
-  'ngRoute',
-  'gmailChecker.home',
+  'ngCookies',
+  'ui.router',
+  'angular-google-gapi',
+  'gmailChecker.router',
+  'gmailChecker.controller'
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+run(['GAuth', 'GApi', 'GData', '$rootScope', '$window', '$state',
+	function(GAuth, GApi, GData, $rootScope, $window, $state) {
+		$rootScope.gdata = GData;
 
-  $routeProvider.otherwise({redirectTo: '/home'});
+        var CLIENT = '630375832656-7e88ud0mb39o3v3agfu2d1qel3a88ps2.apps.googleusercontent.com';
+        var SCOPE = [
+        	"https://www.googleapis.com/auth/userinfo.email",
+        	"https://www.googleapis.com/auth/gmail.readonly"
+        ];
+        SCOPE = SCOPE.join(" ");
+
+        GApi.load('gmail', 'v1');
+
+        GAuth.setClient(CLIENT);
+        GAuth.setScope(SCOPE);
+	    // GAuth.load();
+
+        GAuth.checkAuth().then(
+            function (user) {
+                console.log(user.name + 'is login')
+            },
+            function() {
+            	$state.go("login");
+            }
+        );
+
+        $rootScope.logout = function() {
+            GAuth.logout().then(function () {
+                $cookies.remove('userId');
+                $state.go('login');
+            });
+        };
 }]);
---
+++ app/controller.home.js
@@ -0,0 +1,6 @@
+var controller = angular.module('gmailChecker.controller.home', []);
+
+controller.controller('gmailChecker.controller.home', ['$scope', 'GApi',
+    function homeCtl($scope, GApi) {
+    }
+]);
